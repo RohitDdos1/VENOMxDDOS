c16b1f03902775065d58ba33f66f7025ec56c49e
RohitDdos1 <rohit002676@gmail.com>
2024-07-15 12:03:54 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7249311521:AAHFQTTzXmzEuNyzIJYzloC4EfQ_P3WlvCE' 
+ADMIN_ID = '6591143473'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
